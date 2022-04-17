d6f6d091c8aa9d9d7f085eee1b1051055558b533
Daria <daria.kulepova@gmail.com>
2022-04-17 19:48:46 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+int N;
+
+Console.WriteLine("Input number");
+N=int.Parse(Console.ReadLine());
+
+int count=2;
+
+while (count<=N)
+ {
+      Console.Write($"{count} ");
+      count=count+2;
+  }
+if (N<=1)
+Console.WriteLine("Таких чисел нет");
