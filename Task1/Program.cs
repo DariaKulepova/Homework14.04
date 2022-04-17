4ad187c71cd31e50de42ef40ee04236fb45916fc
Daria <daria.kulepova@gmail.com>
2022-04-17 12:50:54 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+int numberA;
+int numberB;
+
+Console.WriteLine("Input first number");
+numberA=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input second number");
+numberB=int.Parse(Console.ReadLine());
+
+int max=numberA; 
+int min=numberB;
+
+if (numberA>numberB)
+Console.WriteLine($"max={numberA} min={numberB}");
+
+else
+
+Console.WriteLine($"max={numberB} min={numberA}"); 
