c14d05464f3f33a26c5758459e015cc36a03ecea
Daria <daria.kulepova@gmail.com>
2022-04-17 18:24:18 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+int numberA;
+int numberB;
+int numberC;
+
+Console.WriteLine("Input first number");
+numberA=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input second number");
+numberB=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input third number");
+numberC=int.Parse(Console.ReadLine());
+
+int max=numberA;
+
+if (numberB>max) max=numberB;
+if (numberC>max) max=numberC;
+Console.Write("max= ");
+Console.Write(max);
